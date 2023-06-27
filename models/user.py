4f4f4c6f8f3f39137d3c105a45920736f7fa61bc
luongnguy2000 <82204512+luongnguy2000@users.noreply.github.com>
2023-06-27 18:49:24 +0700

    update models

--- models/user.py
+++ models/user.py
@@ -13,3 +13,38 @@ class User(Base):
     email = Column(String)
     password = Column(String)
     date_created = Column(DateTime)
+
+
+class Artist(Base):
+    __tablename__ = "artists"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    bio = Column(String)
+    image_url = Column(String)
+    date_created = Column(DateTime)
+
+
+class Album(Base):
+    __tablename__ = "albums"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    artist_id = Column(Integer, ForeignKey("artists.id"))
+    release_date = Column(DateTime)
+    image_url = Column(String)
+    date_created = Column(DateTime)
+
+    artist = relationship("Artist", backref="albums")
+
+
+class Track(Base):
+    __tablename__ = "tracks"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    album_id = Column(Integer, ForeignKey("albums.id"))
+    duration = Column(Integer)
+    track_number = Column(Integer)
+    date_created = Column(DateTime)
+
+    album = relationship("Album", backref="tracks")
